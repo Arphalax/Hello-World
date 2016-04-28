bfc7ba33cc48fddd2ca7b0b4d010d9af93d684ea
arqdia <arqdia@gmail.com>
2016-04-27 19:22:21 -0500

    mmm
    no compila
    1>------ Build started: Project: LearnC, Configuration: Debug Win32
    ------
    1>  LearnC.cpp
    
    1>c:\users\arqdia\documents\github\hello-world\learnc\learnc\learnc.cpp(16):
    warning C4047: 'function': 'int' differs in levels of indirection from
    'const int *'
    
    1>c:\users\arqdia\documents\github\hello-world\learnc\learnc\learnc.cpp(16):
    warning C4024: 'arrayPrint': different types for formal and actual
    parameter 1
    
    1>c:\users\arqdia\documents\github\hello-world\learnc\learnc\learnc.cpp(31):
    warning C4028: formal parameter 1 different from declaration
    
    1>c:\users\arqdia\documents\github\hello-world\learnc\learnc\learnc.cpp(32):
    error C2057: expected constant expression
    
    1>c:\users\arqdia\documents\github\hello-world\learnc\learnc\learnc.cpp(32):
    error C2466: cannot allocate an array of constant size 0
    
    1>c:\users\arqdia\documents\github\hello-world\learnc\learnc\learnc.cpp(32):
    error C2133: 'arr': unknown size
    ========== Build: 0 succeeded, 1 failed, 0 up-to-date, 0 skipped
    ==========

--- LearnC/LearnC/LearnC.cpp
+++ LearnC/LearnC/LearnC.cpp
@@ -2,10 +2,41 @@
 //
 
 #include "stdafx.h"
+void isEven(int *p1); //Even/odd number check
+void arrayPrint(int p1);
+
 
 
 int main()
 {
+	int x = 6;
+	isEven(&x);
+	int n = 10;
+
+	arrayPrint(&n);
+
+	getchar();
     return 0;
-}
+} // main() end
+
+void isEven(int *p1) {
+	if (*p1 % 2 == 0) {
+		printf("Yes, %d is even\n", *p1);
+	} //if end
+	else {
+		printf("No, %d is odd.\n", *p1);
+	} //else end
+} // isEven() end
+
+void arrayPrint(int *p1) {
+	int arr[*p1];
+	printf("You have a %d element array, and this are the elements values:\n", *p1);
+
+		for (int i = 0; i < *p1; i++) {
+			arr[i] = i;
+			printf("%d", arr[i]);
+
+		} // for end
+
+} // arrayPrint() end
 
