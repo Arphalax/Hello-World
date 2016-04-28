b32c027057904a827e17e55636b1085e1c50ad16
arqdia <arqdia@gmail.com>
2016-04-27 20:02:06 -0500

    arr[]
    No puedo inicializar el array como quiero en la funcion de arrayPrint.

--- LearnC/LearnC/LearnC.cpp
+++ LearnC/LearnC/LearnC.cpp
@@ -2,18 +2,21 @@
 //
 
 #include "stdafx.h"
+#include <stdlib.h>
 void isEven(int *p1); //Even/odd number check
-void arrayPrint(int p1);
+void arrayPrint(int p1); //Print values of Array
+
 
 
 
 int main()
 {
 	int x = 6;
-	isEven(&x);
 	int n = 10;
+	isEven(&x);
 
-	arrayPrint(&n);
+
+	arrayPrint(n);
 
 	getchar();
     return 0;
@@ -28,15 +31,16 @@ void isEven(int *p1) {
 	} //else end
 } // isEven() end
 
-void arrayPrint(int *p1) {
-	int arr[*p1];
-	printf("You have a %d element array, and this are the elements values:\n", *p1);
+void arrayPrint(int p1) {
+	
+	int arr[20]; // Averiguar como hacer para definir el tamaño dinamicamente. !!!!!!!!!!!!
+	printf("You have a %d element array, and this are the elements values:\n", p1);
 
-		for (int i = 0; i < *p1; i++) {
-			arr[i] = i;
-			printf("%d", arr[i]);
+	for (int i = 0; i < p1; i++) {
+		arr[i] = i;
+		printf("%d", arr[i]);
 
-		} // for end
+	} // for end
 
 } // arrayPrint() end
 
